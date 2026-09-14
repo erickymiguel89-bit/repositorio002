791a1b96abcf4eafce53ff1e0c54a80943209902
erickymiguel89-bit <erickymiguel89@gmail.com>
2026-09-14 11:46:12 -0300

    adicionando atividade2

---
+++ project02/src/project02/main.java
@@ -0,0 +1,15 @@
+package project02;
+import javax.swing.JOptionPane;
+public class main {
+public static void main(String[] args) {
+int i = 1;
+int impar = 0;
+while (i <= 25) {
+int a = 1 + impar;
+JOptionPane.showMessageDialog(null, a);
+i++;
+impar++;
+ impar++;	   
+}
+}
+}
